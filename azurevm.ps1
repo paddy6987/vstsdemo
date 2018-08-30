f9b2f3088aa3acf6ce1d0b5d2796b2d184e4b3eb
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-08-30 16:52:02 +0530

    Create azurevm.ps1

---
+++ azurevm.ps1
@@ -0,0 +1,13 @@
+
+
+Connect-AzureRmAccount
+New-AzureRmResourceGroup -Name myResourceGroup -Location EastUS
+New-AzureRmVm `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myVM" `
+    -Location "East US" `
+    -VirtualNetworkName "myVnet" `
+    -SubnetName "mySubnet" `
+    -SecurityGroupName "myNetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -OpenPorts 80,3389
