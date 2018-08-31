52a13f5e42dd1a6a9dfd33c36db58b7ebf670c8f
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-08-31 12:10:14 +0530

    Update azurevm.ps1

--- azurevm.ps1
+++ azurevm.ps1
@@ -1,11 +1,10 @@
 
-
 $username = 'pradnesh'
 $password = "Password@123"
 New-AzureRmResourceGroup -Name myResourceGroup -Location EastUS
 New-AzureRmVm `
     -ResourceGroupName "myResourceGroup" `
-    -Credential ($username,$password)
+    -Credential ($username,$password)`
     -Name "myVM" `
     -Location "East US" `
     -VirtualNetworkName "myVnet" `
