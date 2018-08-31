d32fcedb9b1e9bd4d25a6901cbf4257c73f1d7fa
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-08-31 19:14:50 +0530

    Add files via upload

---
+++ stop_all_AzureVM.ps1
@@ -0,0 +1,11 @@
+﻿##############################################################
+
+##  Script to stop all Azure Virtual Machine.
+
+##   Author = Pradnesh Kambali
+
+##############################################################
+
+Get-AzureRmVM -status | Stop-AzureRmVM 
+
+Write-Host "All Azure VM are stopped"
