6c43a818dcf8a1fcac75e2c965942b6e76568d6a
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-08-31 19:51:21 +0530

    Update stop_all_AzureVM.ps1

--- stop_all_AzureVM.ps1
+++ stop_all_AzureVM.ps1
@@ -5,7 +5,7 @@
 ##   Author = Pradnesh Kambali
 
 ##############################################################
-
+Connect-AzureRmAccount
 Get-AzureRmVM -status | Stop-AzureRmVM 
 
 #Write-Host "All Azure VM are stopped"
